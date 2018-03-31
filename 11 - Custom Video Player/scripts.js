88b6506a3b2b118d7efbb7b122f0d16a121d0209
Islam Dudaev <islamwolf23@gmail.com>
2018-04-01 00:18:18 +0100

    Create handleRange update function
    It will update the volume and the playback rate

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -21,6 +21,10 @@ function skip() {
 	video.currentTime += parseFloat(this.dataset.skip); // we parsing because skip is the string, it should be a number
 }
 
+function handleRangeUpdate() {
+	video[this.name] = this.value;
+}
+
 // Add event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener("play", updateButton);
@@ -28,3 +32,5 @@ video.addEventListener("pause", updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
